fefd139110438471841271374ae078a9a6719166
nabiilrafiq <nabiil.rah@gmail.com>
2023-10-08 07:24:39 +0700

    nambah for statement

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 //tugas javascript 1
 //web D morning
 
+//=============================================if else====================================================//
 // let jam = 20;
 
 // if (jam >= 6 && jam < 12) {
@@ -12,6 +13,8 @@
 //     alert('Selamat Siang')
 // )
 
+
+//===========================================nested if===================================================//
 // let hari = "sabtu";
 
 // if (hari === "senin") {
@@ -24,24 +27,38 @@
 //     console.log("aku gatau ini hari apa")
 // }
 
-let nama = "tes";
-switch (nama) {
-    case "nabiil":
-        console.log("Nama Pertamaku Adalah Nabiil");
-        break;
 
-    case "rafiq":
-        console.log("Nama Keduaku Adalah Rafiq");
-        break;
+//=========================================switch case===============================================//
+// let nama = "tes";
+// switch (nama) {
+//     case "nabiil":
+//         console.log("Nama Pertamaku Adalah Nabiil");
+//         break;
 
-    case "akmali":
-        console.log("Nama Ketigaku Adalah Akmali");
-        break;
+//     case "rafiq":
+//         console.log("Nama Keduaku Adalah Rafiq");
+//         break;
 
-    case "hasan":
-        console.log("Nama Keempatku Adalah Hasan");
-        break;
-    default:
-        console.log("Itu Bukan Namaku!");
-        break;
+//     case "akmali":
+//         console.log("Nama Ketigaku Adalah Akmali");
+//         break;
+
+//     case "hasan":
+//         console.log("Nama Keempatku Adalah Hasan");
+//         break;
+//     default:
+//         console.log("Itu Bukan Namaku!");
+//         break;
+// }
+
+//======================================for statement==================================================//
+const nama = ["Nabiil", "Rafiq", "Akmali", "Hasan"];
+
+let i = 0;
+let len = nama.length;
+let text = "";
+
+for (; i < len;) {
+    console.log(text + nama[i]) + "<br>"
+    i++;
 }
