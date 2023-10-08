4b0bbdfb9d5113b4bf40c5f940615f6273d06551
nabiilrafiq <nabiil.rah@gmail.com>
2023-10-08 07:02:32 +0700

    update script

---
+++ script.js
@@ -0,0 +1,47 @@
+//nabiil rafiq akmali hasan
+//tugas javascript 1
+//web D morning
+
+// let jam = 20;
+
+// if (jam >= 6 && jam < 12) {
+//     alert('Selamat Pagi');
+// }
+
+// else (
+//     alert('Selamat Siang')
+// )
+
+// let hari = "sabtu";
+
+// if (hari === "senin") {
+//     console.log("Ini Adalah Hari Senin")
+// } else if (hari === "selasa") {
+//     console.log("Ini Adalah Hari Selasa")
+// } else if (hari === "rabu") {
+//     console.log("Ini Adalah Hari Rabu")
+// } else {
+//     console.log("aku gatau ini hari apa")
+// }
+
+let nama = "tes";
+switch (nama) {
+    case "nabiil":
+        console.log("Nama Pertamaku Adalah Nabiil");
+        break;
+
+    case "rafiq":
+        console.log("Nama Keduaku Adalah Rafiq");
+        break;
+
+    case "akmali":
+        console.log("Nama Ketigaku Adalah Akmali");
+        break;
+
+    case "hasan":
+        console.log("Nama Keempatku Adalah Hasan");
+        break;
+    default:
+        console.log("Itu Bukan Namaku!");
+        break;
+}
